cf2251ddf4b5b34b87ea639fee831c34dd42c716
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-09-20 13:06:02 +0200

    Proves de GIT

--- proves/UF1/HelloWorld.py
+++ proves/UF1/HelloWorld.py
@@ -1,10 +1,10 @@
 """"
-Rene TUbiera
+Rene Tubiera
 M03 UF1
 20/09/2023
 Exemple de codi Python
 """
 print("Hello World")
-#Traduit al català
+# Traduit al català
 print("Hola món xiquet")
 # TODO Traduir a 3 idiomes més
