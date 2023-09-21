8b9412d626d48074672d8370b1fbcb2aae0c3ec3
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-09-21 11:47:37 +0200

    Exercici 2

---
+++ Exercici 2.py
@@ -0,0 +1,13 @@
+"""
+Rene TUbiera
+M03 UF1
+20/09/2023
+Exercici 2 Comprovar Edat
+"""
+
+# Programa que demsana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+if edat>=18:
+    print("Ets major d'edat")
+print("Programa Finalitzat")
