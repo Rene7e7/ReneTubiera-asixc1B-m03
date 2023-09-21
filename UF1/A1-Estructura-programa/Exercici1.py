974542cb677cfc6dd98a5a6e8f3c021f5ab328dd
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-09-21 12:00:52 +0200

    Exercici 1

--- UF1/A1-Estructura-programa/Exercici1.py
+++ UF1/A1-Estructura-programa/Exercici1.py
@@ -8,6 +8,7 @@ Exercici 1
 #Tenemos que poner la nota que hemos sacado en el examen
 NotaExamen1 = int(input("Dime la nota del primer Examen: "))
 NotaExamen2 = int(input("Dime la Nota del segundo Examen: "))
+
 #Comprueba si ha aprobado los 2 examenes
 if NotaExamen2 < 5 or NotaExamen1 < 5:
     print("No puedo calcular la nota final porque has suspendido algun examen ")
