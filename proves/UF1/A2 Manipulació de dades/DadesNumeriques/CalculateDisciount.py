8aef16f38edfaa9690d92dcf4e2847423a992206
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-09-29 07:39:51 +0200

    Exemple del apunts

--- proves/UF1/A2 Manipulació de dades/DadesNumeriques/CalculateDisciount.py
+++ proves/UF1/A2 Manipulació de dades/DadesNumeriques/CalculateDisciount.py
@@ -2,4 +2,3 @@ PreuActual = float(input("Que precioA tiene? "))
 PreuOriginal = float(input("Que precioO tiene? "))
 descompte = PreuOriginal - PreuActual * PreuOriginal
 print("Tiene un descunto de ",descompte)
-print
