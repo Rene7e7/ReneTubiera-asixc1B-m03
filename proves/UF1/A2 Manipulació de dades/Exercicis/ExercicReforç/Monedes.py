b6672b43cde31db22342c8faae73f58970aa7240
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-10-05 13:06:16 +0200

    Exercici Classe

---
+++ proves/UF1/A2 Manipulació de dades/Exercicis/ExercicReforç/Monedes.py
@@ -0,0 +1,14 @@
+""""
+Rene Tubiera
+M03 UF1
+20/09/2023
+quantitat de monedes
+"""
+
+a = int(input("Quantes monedes de 2 Euros: "))
+b = int(input("Quantes monedes de 1: "))
+c = int(input("Quantes monedes de 0.50: "))
+d = int(input("Quantes monedes de 0.10: "))
+e = int(input("Quantes monedes de 0.05: "))
+
+print(a * 2 + b * 1 + c * 0.5 + d * 0.1 + e * 0.05)
---
+++ proves/UF1/A2 Manipulació de dades/Exercicis/ExerciciClasse/Notes.py
@@ -0,0 +1,13 @@
+
+entrada = input("AdreçaIP: ")
+AdreçaIP = entrada.split()
+print(AdreçaIP[0])
+
+
+""""
+entrada = input("notes: ")
+notes = entrada.split()
+print(notes[0])
+doble = float(notes[0])*2
+print(doble)
+"""
