7fad2e8d02f75a1aecd9796d31c6a4110f904b60
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-10-19 12:16:19 +0200

    ExReforç

--- pp1-TubieraRene-ASIXb1/e1.py
+++ pp1-TubieraRene-ASIXb1/e1.py
@@ -3,9 +3,12 @@ Rene Tubiera Sadinas
 19/10/23
 M03
 UF1
-Calcul diagonal
+Pp1B Prova Practica
+Exercici: calcula la diagonal d'un rectangle
 """
+# Fem un import math per fer el sqrt()
 import math
+
 # Demanem a l'usuari que doni un valor a l'alçada i amplada
 lenght = int(input("Donem un valor per el alçada"))
 width = int(input("Donem un valor per el amplada"))
@@ -13,5 +16,5 @@ width = int(input("Donem un valor per el amplada"))
 # Es el calcul del diagonal de un rectangle
 diagonal = math.sqrt(lenght**2+width**2)
 
-# Mostra per pantalla le resulta del calcul utilitzant el variable diagonal
+# Mostra per pantalla el resultat del cálcul utilitzant el variable diagonal
 print("El resultat del calcul del diagonal del rectangle es: ", diagonal)
--- pp1-TubieraRene-ASIXb1/e2.py
+++ pp1-TubieraRene-ASIXb1/e2.py
@@ -4,7 +4,7 @@ Rene Tubiera Sadinas
 M03
 UF1
 Pp1B Prova Practica
-Exercici Nota Final de la UF1 de 3 estudiants
+Exercici nota final de la UF1 del mòdul 03 del Cicle de Grau Superior d'Administració de Sistemes Informàtics en Xarxa (CFGS ASIX)
 """
 # Demanem a l'usuari que posi les Notes de la RA1 i RA2 de cada estudiant
 RA1_Estudiant1 = int(input("Donam la nota del RA1 del estudiant 1: "))
@@ -14,12 +14,12 @@ RA2_Estudiant1 = int(input("Donam la nota del RA1 del estudiant 1: "))
 RA2_Estudiant2 = int(input("Donam la nota del RA1 del estudiant 2: "))
 RA2_Estudiant3 = int(input("Donam la nota del RA1 del estudiant 3: "))
 
-# Calcul de la QUF1 de cada estudiant
+# Calcul de la QUF1 (QUF1 = 0,30*RA1 + 0,70*RA2) de cada estudiant
 QUF1_Estudiant1 = 0.30 * RA1_Estudiant1 + 0.70 * RA2_Estudiant1
 QUF1_Estudiant2 = 0.30 * RA1_Estudiant2 + 0.70 * RA2_Estudiant2
 QUF1_Estudiant3 = 0.30 * RA1_Estudiant3 + 0.70 * RA2_Estudiant3
 
-# mostra per pantalla el resultat pero en
+# mostra per pantalla el resultat de la Nota Final de la UF1 de cada estudiant peró en 2 decimals
 print("La Nota final de la UF1 del estudiant 1 es: ", int(QUF1_Estudiant1))
 print("La Nota final de la UF1 del estudiant 2 es: ", int(QUF1_Estudiant2))
 print("La Nota final de la UF1 del estudiant 3 es: ", int(QUF1_Estudiant3))
