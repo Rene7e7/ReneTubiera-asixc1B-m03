c76f5555bc95d98df21b7611e44b0c6371c0db4c
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2023-11-23 11:48:35 +0100

    A4

--- proves/UF1/A4 Estructures de repeticio/Exercicis classe/piramide.py
+++ proves/UF1/A4 Estructures de repeticio/Exercicis classe/piramide.py
@@ -2,7 +2,7 @@ try:
     numero = int(input("Dime numero: "))
     for i in range(0, numero):
         i+=1
-        print('# ' * i)
+        print('🐍 ' * i)
 except ValueError:
     print("Error")
 
