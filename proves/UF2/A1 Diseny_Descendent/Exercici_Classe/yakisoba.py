4424c1adf1a59a54ac812e7604796d782feaf4df
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-02-07 12:59:13 +0100

    REFORÇ

---
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/yakisoba.py
@@ -0,0 +1,64 @@
+from time import sleep
+# region Declaració de Funcions ----------
+def recopilar_ingredients():
+   print("Comprar al supermercat")
+   sleep(1)
+   print("Disposar-los sobre el marbre")
+   sleep(1)
+# endregion
+# region INICI: Preparar fideus Yakisoba
+recopilar_ingredients()
+# endregion
+
+# region Cuinar Tallarines
+def Cuinar_tallarines():
+   print("Preparar Aigua")
+   sleep(1)
+   print("Bullir tallarines")
+   sleep(1)
+   print("Esccores Tallarines")
+   sleep(1)
+   print("Deixar-les preparades")
+   sleep(1)
+
+# endregion
+# region INICI: Preparar fideus Yakisoba
+Cuinar_tallarines()
+
+
+# region Cuinar Pastanagues
+def Cuinar_Pastanagues():
+   print("Tallar patanagues")
+   sleep(1)
+   print("Fregir pastanagues")
+   sleep(1)
+   print("Deixar pastanagues preparades")
+   sleep(1)
+# endregion
+# region INICI: Preparar fideus Yakisoba
+Cuinar_Pastanagues()
+
+# region Cuinar Cebes
+def Cuinar_Cebes():
+   print("Tallar Cebes")
+   sleep(1)
+   print("Fregir Cebes")
+   sleep(1)
+   print("Deixar cebes Preparades")
+   sleep(1)
+# endregion
+# region INICI: Preparar fideus Yakisoba
+Cuinar_Cebes()
+
+# region Preparació Final
+def Preparacio_Final():
+   print("Barrejar ingredients amb salsa yakitori")
+   sleep(1)
+   print("Saltar ingredients")
+   sleep(1)
+   print("Deixar llest per servir")
+   sleep(1)
+# endregion
+# region INICI: Preparar fideus Yakisoba
+Preparacio_Final()
+
