bc4d85e155de5298f5f08f269abc3d39971c22e7
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-02-13 11:30:47 +0100

    REFORÇ

--- proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchAnalizer.py
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchAnalizer.py
@@ -18,74 +18,26 @@ Donat els diferents resultats analitza que està passant.
 -1
 L'usuari introduirà primer el nom dels dos equips i després el resultat actual
 '''
-# Obtenir els noms dels equips
-equip_local = input()
-equip_visitant = input()
-# diferents resultats
-a = 0
-b = 0
-Inicio_puntuacio = (a,b)
+# region Equips
+def equips():
+    equip1 = input("Introdueix el nom del primer equip: ")
+    equip2 = input("Introdueix el nom del segon equip: ")
 
-puntuacio = input()
-
-# region Equip
-def equip():
-    equip_local = input()
-    equip_visitant = input()
-# endregion
-# region puntuacio
-def puntuacio_partit():
-    Puntuacio_A = []
-    Puntuacio_B = []
-
-    # Definimos las puntuaciones anteriores
-    puntuacio_anterior_A = 0
-    puntuacio_anterior_B = 0
-
+    punts_equip1 = 0
+    punts_equip2 = 0
     while True:
-        entrada = input(
-            "Introduce la puntuación de A seguida de la puntuación de B (separadas por un espacio), o bien introduce -1 para finalizar: ")
-        puntuacio = entrada.split()
-        if len(puntuacio) >= 2:
-            # Si la puntuación no es -1, actualizamos la puntuación anterior
-            if puntuacio[0] != '-1':
-                puntuacio_anterior_A = int(puntuacio[0])
-            if puntuacio[1] != '-1':
-                puntuacio_anterior_B = int(puntuacio[1])
-
-            # Añadimos las puntuaciones a las listas
-            Puntuacio_A.append(puntuacio_anterior_A)
-            Puntuacio_B.append(puntuacio_anterior_B)
-
-            # Si se introduce -1, salimos del bucle
-            if puntuacio[0] == '-1':
+        try:
+            punts_equip1, punts_equip2 = map(int, input(f"Introdueix els punts ({equip1} {equip2}): ").split())
+            if punts_equip1 == -1 or punts_equip2 == -1:
                 break
-        else:
-            print("Por favor, introduce dos puntuaciones separadas por un espacio.")
-
+        except ValueError:
+            print("Per favor, introdueix dos nombres separats per un espai.")
+            break
 # endregion
-# region Resultat
-def resulta():
-    Resultat_A = []
-    Resultat_B = []
 
-# endregion
-# region Guanyador
-def guanyador():
+# region puntuacio
+def puntuacio():
     pass
 # endregion
-
-# region Funcions
-equip()
-resulta()
-guanyador()
-puntuacio_partit()
-
-'''
-puntos == 3:
-    print("Es un triple)
-puntos == 2:
-    print("Es una cistella)
-puntos == 1:
-    print("Es un punto de tir lliure)
-'''
+equips()
+puntuacio()
--- proves/UF2/A1 Diseny_Descendent/Exercici_Classe/Puntuacio.py
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/Puntuacio.py
@@ -1,28 +1,44 @@
-Puntuacio_A = []
-Puntuacio_B = []
+def analyze_game():
+    team1 = input("Enter the name of the first team: ")
+    team2 = input("Enter the name of the second team: ")
 
-# Definimos las puntuaciones anteriores
-puntuacio_anterior_A = 0
-puntuacio_anterior_B = 0
+    team1_score = 0
+    team2_score = 0
 
-while True:
-    entrada = input(
-        "Introduce la puntuación de A seguida de la puntuación de B (separadas por un espacio), o bien introduce -1 para finalizar: ")
-    puntuacio = entrada.split()
-    if len(puntuacio) >= 2:
-        # Si la puntuación no es -1, actualizamos la puntuación anterior
-        if puntuacio[0] != '-1':
-            puntuacio_anterior_A = int(puntuacio[0])
-        if puntuacio[1] != '-1':
-            puntuacio_anterior_B = int(puntuacio[1])
-
-        # Añadimos las puntuaciones a las listas
-        Puntuacio_A.append(puntuacio_anterior_A)
-        Puntuacio_B.append(puntuacio_anterior_B)
-
-        # Si se introduce -1, salimos del bucle
-        if puntuacio[0] == '-1':
+    while True:
+        score = input("Enter the current score (or -1 to end): ")
+        if score == "-1":
             break
-    else:
-        print("Por favor, introduce dos puntuaciones separadas por un espacio.")
 
+        score1, score2 = map(int, score.split())
+        if score1 > team1_score:
+            if score1 - team1_score == 1:
+                print(f"Free kick of {team1}")
+            elif score1 - team1_score == 2:
+                print(f"Basket of {team1}")
+            elif score1 - team1_score == 3:
+                print(f"Triple of {team1}")
+            else:
+                print(f"Score of {score1 - team1_score} points by {team1}")
+
+            team1_score = score1
+        elif score2 > team2_score:
+            if score2 - team2_score == 1:
+                print(f"Free kick of {team2}")
+            elif score2 - team2_score == 2:
+                print(f"Basket of {team2}")
+            elif score2 - team2_score == 3:
+                print(f"Triple of {team2}")
+            else:
+                print(f"Error")
+
+            team2_score = score2
+
+    if team1_score > team2_score:
+        print(f"{team1} wins")
+    elif team2_score > team1_score:
+        print(f"{team2} wins")
+    else:
+        print("The game is a draw")
+
+analyze_game()
