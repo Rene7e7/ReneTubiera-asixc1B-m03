8eb13b09fe77663050abd40d6f774a6f12d2a636
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-02-14 12:43:45 +0100

    UF2 match

--- proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchAnalizer.py
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchAnalizer.py
@@ -1,1 +1,43 @@
- of the second team: ")
+def equips():
+    equipo1 = input("")
+    equipo2 = input("")
+    return equipo1, equipo2
+
+def puntuacio(equipo1, equipo2):
+    puntuacio_equipo1 = 0
+    puntuacio_equipo2 = 0
+    while True:
+        score = input("")
+        if score == "-1":
+            break
+        # Assuming score is a string in the format 'team1:team2'
+        score1, score2 = map(int, score.split(' '))
+        if score1 > puntuacio_equipo1:
+            if score1 - puntuacio_equipo1 == 1:
+                print(f"Lliure de {equipo1}")
+            elif score1 - puntuacio_equipo1 == 2:
+                print(f"Cistella de {equipo1}")
+            elif score1 - puntuacio_equipo1 == 3:
+                print(f"Triple de {equipo1}")
+            puntuacio_equipo1 = score1
+        elif score2 > puntuacio_equipo2:
+            if score2 - puntuacio_equipo2 == 1:
+                print(f"Lliure de {equipo2}")
+            elif score2 - puntuacio_equipo2 == 2:
+                print(f"Cistella de {equipo2}")
+            elif score2 - puntuacio_equipo2 == 3:
+                print(f"Triple de {equipo2}")
+            puntuacio_equipo2 = score2
+    return puntuacio_equipo1, puntuacio_equipo2
+
+def guanyador(puntuacio_equipo1, puntuacio_equipo2, equipo1, equipo2):
+    if puntuacio_equipo1 > puntuacio_equipo2:
+        print(f"Guanya {equipo1}")
+    elif puntuacio_equipo2 > puntuacio_equipo1:
+        print(f"Guanya {equipo2}")
+    else:
+        print("Empat")
+
+equipo1, equipo2 = equips()
+puntuacio_equipo1, puntuacio_equipo2 = puntuacio(equipo1, equipo2)
+guanyador(puntuacio_equipo1, puntuacio_equipo2, equipo1, equipo2)
--- proves/UF2/A1 Diseny_Descendent/Exercici_Classe/Puntuacio.py
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/Puntuacio.py
@@ -1,13 +1,13 @@
-'''
+
 def analyze_game():
-    team1 = input("Enter the name of the first team: ")
-    team2 = input("Enter the name of the second team: ")
+    team1 = input("")
+    team2 = input("")
 
     team1_score = 0
     team2_score = 0
 
     while True:
-        score = input("Enter the current score (or -1 to end): ")
+        score = input("")
         if score == "-1":
             break
 
@@ -43,52 +43,3 @@ def analyze_game():
         print("The game is a draw")
 
 analyze_game()
-'''
-def equips():
-    equipo1 = input("Enter the name of team 1: ")
-    equipo2 = input("Enter the name of team 2: ")
-    return equipo1, equipo2
-
-def puntuacio():
-    puntuacio_equipo1 = 0
-    puntuacio_equipo2 = 0
-    while True:
-        score = input("Enter the current score (or -1 to end): ")
-        if score == "-1":
-            break
-        # Assuming score is a string in the format 'team1:team2'
-        score1, score2 = map(int, score.split(':'))
-        if score1 > puntuacio_equipo1:
-            if score1 - puntuacio_equipo1 == 1:
-                print(f"Free kick of {equipo1}")
-            elif score1 - puntuacio_equipo1 == 2:
-                print(f"Basket of {equipo1}")
-            elif score1 - puntuacio_equipo1 == 3:
-                print(f"Triple of {equipo1}")
-            else:
-                print(f"Score of {score1 - puntuacio_equipo1} points by {equipo1}")
-            puntuacio_equipo1 = score1
-        elif score2 > puntuacio_equipo2:
-            if score2 - puntuacio_equipo2 == 1:
-                print(f"Free kick of {equipo2}")
-            elif score2 - puntuacio_equipo2 == 2:
-                print(f"Basket of {equipo2}")
-            elif score2 - puntuacio_equipo2 == 3:
-                print(f"Triple of {equipo2}")
-            else:
-                print(f"Score of {score2 - puntuacio_equipo2} points by {equipo2}")
-            puntuacio_equipo2 = score2
-    return puntuacio_equipo1, puntuacio_equipo2
-
-def guanyador(puntuacio_equipo1, puntuacio_equipo2, equipo1, equipo2):
-    if puntuacio_equipo1 > puntuacio_equipo2:
-        print(f"{equipo1} wins")
-    elif puntuacio_equipo2 > puntuacio_equipo1:
-        print(f"{equipo2} wins")
-    else:
-        print("The game is a draw")
-
-equipo1, equipo2 = equips()
-puntuacio_equipo1, puntuacio_equipo2 = puntuacio()
-guanyador(puntuacio_equipo1, puntuacio_equipo2, equipo1, equipo2)
-
