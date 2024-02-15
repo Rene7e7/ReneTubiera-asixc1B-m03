e5291dc5fad9f72d4d7f44013c91591ccef671f6
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-02-15 13:00:42 +0100

    exercici Classe

---
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/Linux Commands.py
@@ -0,0 +1,119 @@
+'''
+Al ITB s'està creant un nou sistema operatiu, basat el Linux. De moment, només té unes poques comandes, donat que és una versió alfa.
+
+Les comandes que es poden executar són:
+
+touch
+
+grep
+
+cat
+
+fdisk
+
+cmp
+
+dmesg
+
+man
+
+top
+
+htop
+
+halt   "Cas especial" Aquesta comanda aturarà el sistema, tot i que no l'apagarà
+
+Les opcions que han de tenir implementades per aquesta versió són:
+
+-h, --help
+
+           Mostra un text d'ajuda i surt
+
+ -v, --version
+
+          Mostra la versió i surt
+
+En cas de triar una opció no valida ha de mostrar el missatge: Value xx is not valid option BACALAO"
+
+A on les xx ha de ser l'opció triada que no ha estat valida
+
+Input
+fdisk --help
+
+Output
+fdisk is a dialog-driven program for creation and manipulation of partition tables.
+
+It understands GPT, MBR, Sun, SGI and BSD partition tables.
+
+
+
+Block devices can be divided into one or more logical disks called partitions.
+
+This division is recorded in the partition table, usually found in sector 0 of the disk.
+
+(In the BSD world one talks about `disk slices' and a `disklabel'.)
+
+.  .  .
+
+
+Input
+fdisk -v
+
+Output
+v0.1
+
+
+
+
+Input
+fdisk -a
+
+Output
+Value -a is not valid option, "User BACALAO"
+
+
+Input
+ls -h
+
+Output
+ls comand not found, "User BACALAO"
+
+
+EXTRA:
+
+Com a benvinguda, fer que mostri una frase cada vegada que s'executi l'aplicació.
+
+La frase hauria de ser aleatòria.
+
+
+CONSELL per a "Cavernícoles"
+
+Más de 400 comandos para GNU/Linux que deberías conocer
+'''
+
+# region variables
+Llista_Comandes= ["touch", "grep", "cat", "fdisk", "cmp", "dmesg", "man", "top", "htop","halt"]
+Llista_Opcions = ["-h", "--help", "-v", "--version"]
+Usuari = "USER@BACALAO"
+Historial = []
+# endregion
+
+# region entrada comandas
+def Comanda():
+    Entrada_Terminal = input("USER@BACALAO:~$ ")
+    Separat = Entrada_Terminal.split()
+    Historial.append(Separat)
+    return Historial  # Retrona la lista
+# endregion
+
+# region Comprovacio comanda
+def Comprovacio():
+        if Historial[0][0] in Llista_Comandes and Historial[0][1] in Llista_Opcions:
+            print("Les Comandes si existeix")
+        else:
+            print("Les Comandes no existeix")
+
+# endregion
+
+Comanda()
+Comprovacio()
--- proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchBasket/MatchAnalizer(Otra Forma).py
+++ proves/UF2/A1 Diseny_Descendent/Exercici_Classe/MatchBasket/MatchAnalizer(Otra Forma).py
@@ -1,6 +1,34 @@
-# region equips
+
+# region Funcio Equips
 def equips():
     equipo1 = input("")
     equipo2 = input("")
     return equipo1, equipo2
 # endregion
+# region Funcio puntuacio
+def puntuacio(equipo1, equipo2):
+    pass
+# endregion
+# region Funcio comparacio
+def comparacio():
+    pass
+# endregion
+# region Funcio resultat
+def resultat():
+    pass
+# endregion
+# region Funcio Guanyador
+def guanyador():
+    pass
+# endregion
+# region Funions Partits
+equipo1, equipo2 = equips()
+puntuacio()
+comparacio()
+resultat()
+guanyador()
+
+
+
+# endregion
+
