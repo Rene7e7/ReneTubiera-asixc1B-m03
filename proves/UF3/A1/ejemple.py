80f8ba243eb3f8bdbaa4921f4139328f0757438d
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-04-10 12:00:58 +0200

    projecte

---
+++ proves/UF3/A1/ejemple.py
@@ -0,0 +1,5 @@
+f = open('un_poble.txt', mode='wt', encoding='utf-8')
+f.write('La mare era mestra.\n')
+
+f.write('Vivíem tots dos sols. No teníem ni vàter.\nDamunt de la casa, les estrelles')
+f.close()
