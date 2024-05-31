5c0deea6c99f920a9903a81ea6cac1b1c5ba2f70
rene.tubiera.7e7 <rene.tubiera.7e7@itb.cat>
2024-05-31 11:27:28 +0200

    recu uf4

--- proves/UF3 Recu/A1. Fitxers E_S Exercici Reforç/comptar_paraules.fitxer/conteo.py
+++ proves/UF3 Recu/A1. Fitxers E_S Exercici Reforç/comptar_paraules.fitxer/conteo.py
@@ -1,5 +1,11 @@
 def comptar_paraules(frases):
+    # la funció enumerate() retorna una tupla amb un comptador i un element de la llista
+    # linia per linia anem comptant les paraules
+    # frase lo que fa és recórrer la llista frases
+    # i és el comptador
     for i, frase in enumerate(frases):
+        # strip lo que fa es eliminar els espais en blanc al principi i al final de la cadena
         if frase.strip() == 'FI':
             break
+        # i + 1 perquè la primera línia és la 1 i no la 0
         print(f'La línia {i+1} té {len(frase.split())} paraules.')
