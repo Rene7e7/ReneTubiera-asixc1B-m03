0e312d737ce009cfd10ef130247f13a34cedbdd2
Rene7e7 <rene.tubiera.7e7@itb.cat>
2024-06-01 23:13:17 +0200

    RECU UF4

--- proves/UF3 Recu/A1. Fitxers E_S Exercici Reforç/text_al_arxiu/intercambiar_mayusculas_minusculas.py
+++ proves/UF3 Recu/A1. Fitxers E_S Exercici Reforç/text_al_arxiu/intercambiar_mayusculas_minusculas.py
@@ -1,5 +1,6 @@
 def intercambiar_mayusculas_minusculas(texto):
     try:
+        # Intercambiar mayúsculas y minúsculas
         return texto.swapcase()
     except Exception as e:
         print(f"Error al intercambiar mayúsculas y minúsculas: {e}")
